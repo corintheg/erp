68953455fd80dbe32b02f8e9c4c4ee8242d49a7a
41M3Dev <102484378+41M3Dev@users.noreply.github.com>
2025-03-26 11:06:06 +0100

    Feat : correction de la création de compte

--- app/Models/Utilisateur.php
+++ app/Models/Utilisateur.php
@@ -11,7 +11,12 @@ class Utilisateur extends Authenticatable
 
     protected $primaryKey = 'id_utilisateur';
 
-    protected $fillable = ['username', 'mot_de_passe'];
+    protected $fillable = ['id_employe',
+        'username',
+        'mot_de_passe',
+        'email',
+        'date_creation',
+        'date_modification'];
 
     protected $hidden = ['mot_de_passe'];
 
