56266e481d5e912c15063aeb16fa199d93e24b1f
Corinthe <corinthe.guerain@gmail.com>
2025-04-01 09:41:27 +0200

    feat: add Poppins font for improved typography across user interface

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,11 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <style>
         /* Animation pour la sidebar */
         @media (max-width: 768px) {
@@ -60,6 +65,10 @@
                 opacity: 1;
                 transform: translateY(0);
             }
+        }
+
+        * {
+            font-family: 'Poppins', sans-serif;
         }
     </style>
 </head>
